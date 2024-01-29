9c037bc2ffc53317f91a5b0c627150d9822b11c4
FIRST_NAME LAST_NAME <tomasxrandi@hotmail.se>
2024-01-29 10:07:04 +0100

    Chapter 1

--- src/vk_engine.h
+++ src/vk_engine.h
@@ -1,39 +1,70 @@
 ﻿#pragma once
 
-#include <vk_types.h>
+#include "vk_types.h"
 
-class VulkanEngine
-{
+class VulkanEngine {
 public:
-	bool _isInitialized{false};
-	int _frameNumber{0};
-	bool stop_rendering{false};
-	VkExtent2D _windowExtent{1700, 900};
+  bool _isInitialized{false};
+  int _frameNumber{0};
+  bool stop_rendering{false};
+  VkExtent2D _windowExtent{1700, 900};
 
-	struct SDL_Window *_window{nullptr};
+  struct SDL_Window *_window{nullptr};
 
-	static VulkanEngine &Get();
+  static VulkanEngine &Get();
 
-	VkInstance _instance;
-	VkDebugUtilsMessengerEXT _debug_messenger;
-	VkDevice _device;
-	VkPhysicalDevice _chosenGPU;
-	VkSurfaceKHR _surface;
-	// initializes everything in the engine
-	void init();
+  // basic pieces
+  VkInstance _instance;
+  VkDebugUtilsMessengerEXT _debug_messenger;
+  VkDevice _device;
+  VkPhysicalDevice _chosenGPU;
+  VkSurfaceKHR _surface;
 
-	// shuts down the engine
-	void cleanup();
+  // swapchain
+  VkSwapchainKHR _swapchain;
+  VkFormat _swapchain_image_format;
 
-	// draw loop
-	void draw();
+  std::vector<VkImage> _swapchain_images;
+  std::vector<VkImageView> _swapchain_image_views;
+  VkExtent2D _swapchain_extent;
 
-	// run main loop
-	void run();
+  // Frame stuff
+  FrameData _frames[FRAME_OVERLAP];
+  FrameData &get_current_frame() {
+    return _frames[_frameNumber % FRAME_OVERLAP];
+  };
+
+  VkQueue _graphicQueue;
+  uint32_t _graphicQueueFamily;
+
+  // initializes everything in the engine
+  void init();
+
+  // shuts down the engine
+  void cleanup();
+
+  // draw loop
+  void draw();
+
+  // run main loop
+  void run();
 
 private:
-	void init_vulkan();
-	void init_swapchain();
-	void init_commands();
-	void init_sync_structures();
+  void init_vulkan();
+  void init_swapchain();
+  void init_commands();
+  void init_sync_structures();
+
+  void create_swapchain(uint32_t width, uint32_t height);
+  void destroy_swapchain();
 };
+
+struct FrameData {
+  VkCommandPool _commandPool;
+  VkCommandBuffer _mainCommandBuffer;
+
+  VkSemaphore _swapchainSemaphore, _renderSemaphore;
+  VkFence _renderFence;
+};
+
+constexpr unsigned int FRAME_OVERLAP = 2;
--- src/vk_images.cpp
+++ src/vk_images.cpp
@@ -1,1 +1,32 @@
 #include <vk_images.h>
+
+void vkutil::transition_image(VkCommandBuffer cmd, VkImage image,
+                              VkImageLayout currentLayout,
+                              VkImageLayout newLayout) {
+
+  VkImageAspectFlags aspectMask =
+      (newLayout == VK_IMAGE_LAYOUT_DEPTH_ATTACHMENT_OPTIMAL)
+          ? VK_IMAGE_ASPECT_DEPTH_BIT
+          : VK_IMAGE_ASPECT_COLOR_BIT;
+
+  VkDependencyInfo depInfo{
+      .sType = VK_STRUCTURE_TYPE_DEPENDENCY_INFO,
+  };
+
+  VkImageMemoryBarrier2 imageBarrier{
+      .sType = VK_STRUCTURE_TYPE_MEMORY_BARRIER_2,
+      .pNext = nullptr,
+      .srcStageMask = VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT,
+      .srcAccessMask = VK_ACCESS_2_MEMORY_WRITE_BIT,
+      .dstStageMask = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT,
+      .dstAccessMask =
+          VK_ACCESS_2_MEMORY_WRITE_BIT | VK_ACCESS_2_MEMORY_READ_BIT,
+      .oldLayout = currentLayout,
+      .newLayout = newLayout,
+
+  };
+  depInfo.pNext = nullptr;
+  depInfo.imageMemoryBarrierCount = 1;
+
+  depInfo.pImageMemoryBarriers = &imageBarrier;
+}
